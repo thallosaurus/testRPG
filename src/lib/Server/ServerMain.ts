f39603975ab9bb8f5bcc680ebebbd92fe86b18f0
Philipp Gaßner <activator112233@gmail.com>
2021-06-26 17:41:34 +0200

    removed server.js, src/lib/Server/ServerMain.ts is the new replacement

--- src/lib/Server/ServerMain.ts
+++ src/lib/Server/ServerMain.ts
@@ -1,17 +1,38 @@
 import { Server, Socket } from 'socket.io';
 import http from 'http';
 import { Player } from './Player';
+import express from 'express';
 
 export namespace MultiplayerServer {
+    const WEBMANIFEST = {
+        "dir": "ltr",
+        "lang": "en-US",
+        "name": "schrottimon",
+        "short_name": "schrottimon",
+        "description": "schrottimon",
+        "icons": [{
+            "src": "/assets/icon_512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+        }],
+        "background_color": "#000000",
+        "theme_color": "#000000",
+        "display": "standalone",
+        "orientation": "landscape-primary"
+    }
+
     export class ServerMain {
         private readonly http
         private readonly io: Server;
         private readonly port: number = 4000;
+        private readonly expressApp: express.Application;
         constructor() {
-            this.http = http.createServer();
+            this.expressApp = express();
+            this.http = http.createServer(this.expressApp);
             this.io = new Server(this.http);
-
             this.setupSocket(this.io);
+            process.chdir("..");
+            this.setupRoutes();
 
             this.http.listen(this.port, () => {
                 console.log("Socket Server is listening to Port " + this.port);
@@ -25,6 +46,35 @@ export namespace MultiplayerServer {
                 });
             });
         }
+
+        private setupRoutes() {
+            this.expressApp.get("/main.js", (req: express.Request, res: express.Response, next) => {
+                console.log("[DEVSERVER] Sending main client bundle");
+                res.sendFile("client.bundle.js", { root: __dirname });
+                // res.sendFile("client.bundle.js");
+            });
+
+            this.expressApp.use("/assets", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+                console.log(`[DEVSERVER] Sending Sprite ${req.originalUrl}`);
+                next();
+            }, express.static(__dirname + "/../assets"));
+
+            this.expressApp.use("/src", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+                console.log(`[DEVSERVER] Sending Source ${req.originalUrl}`);
+                next();
+            }, express.static(__dirname + "/../src"));
+
+            this.expressApp.get("/", (req: express.Request, res: express.Response) => {
+                console.log(__dirname);
+                res.sendFile("./devassets/index.html", { root: __dirname + "/.."});
+            });
+
+            this.expressApp.get("/manifest.webmanifest", (req, res) => {
+                res.send(WEBMANIFEST);
+            });
+        }
     }
+
+    console.log(__dirname);
     new ServerMain();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/module.ts',
+const serverConfig = {
+    target: 'node',
+    entry: './src/lib/Server/ServerMain.ts',
     devtool: 'inline-source-map',
     context: __dirname,
     module: {
         rules: [{
             test: /\.ts$/,
             use: 'ts-loader',
-            exclude: "/node_modules/"
+            exclude: path.resolve(__dirname, "node_modules/")
         }]
     },
     resolve: {
@@ -16,6 +17,60 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: 'server.bundle.js'
     }
 }
+
+const clientConfig = {
+    entry: './src/module.ts',
+    devtool: 'inline-source-map',
+    context: __dirname,
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: path.resolve(__dirname, "node_modules/")
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'client.bundle.js'
+    }
+}
+
+module.exports = [serverConfig, clientConfig]; /* {
+    entry: {
+        client: './src/module.ts',
+        server: './src/lib/Server/ServerMain.ts'
+    },
+    devtool: 'inline-source-map',
+    context: __dirname,
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: path.resolve(__dirname, "node_modules/")
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        fallback: {
+            "http": false,
+            "url": false,
+            "crypto": false,
+            "path": false,
+            "stream": false,
+            "zlib": false,
+            "fs": false,
+            "querystring-es3": false,
+            "querystring": false
+        }
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    }
+} */
